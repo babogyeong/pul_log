a76bc02c977203bc1ad1f3395a386117da330111
babogyeong <122768710+babogyeong@users.noreply.github.com>
2025-08-21 13:17:49 +0900

    Update onboarding.js

--- pul_log/onboarding/onboarding.js
+++ pul_log/onboarding/onboarding.js
@@ -1,5 +1,5 @@
 /* ===== 설정 ===== */
-const NEXT_URL = "/pul_log/intro/intro.html"; // 온보딩 후 이동할 페이지 (필요 시 변경)
+const NEXT_URL = "https://d3p96xzg4kgeam.cloudfront.net/intro/intro.html"; // 온보딩 후 이동할 페이지 (필요 시 변경)
 const HOLD_MS = getComputedStyle(document.documentElement)
   .getPropertyValue("--hold")
   .trim();
